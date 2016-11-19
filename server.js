f369d3e6ffa9dd273dff6789b7fb42722cd28522
Andrew Kolander <akolander22@gmail.com>
2016-11-19 12:32:02 -0600

    loading angular

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 
 var app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', function(request, response){
   response.sendFile(path.join(__dirname, 'public/views/index.html'));
 })
