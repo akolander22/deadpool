75e7974583f83c901a6fecc4ee31b46cfe8af12a
Andrew Kolander <akolander22@gmail.com>
2016-11-20 16:18:33 -0600

    added main controller

---
+++ public/scripts/MainController.js
@@ -0,0 +1,5 @@
+angular.module('deadApp').controller('MainController', function($http){
+
+
+console.log("Hello");
+});
--- public/scripts/deadApp.config.js
+++ public/scripts/deadApp.config.js
@@ -1,6 +1,6 @@
 angular.module('deadApp').config(function($routeProvider, $locationProvider){
   $routeProvider
-
+  
 
 
   $locationProvider.html5Mode(true);
