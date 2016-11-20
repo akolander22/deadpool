f06682a5acb31a6cbb82f40945862c366aec9d56
Andrew Kolander <akolander22@gmail.com>
2016-11-20 16:21:11 -0600

    starting search

--- public/scripts/MainController.js
+++ public/scripts/MainController.js
@@ -1,5 +1,9 @@
 angular.module('deadApp').controller('MainController', function($http){
+  var vm = this;
 
+  vm.submit = function(){
 
-console.log("Hello");
+    console.log("searching for ", vm.search)
+  }
+
 });
