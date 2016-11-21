77bb72d35db09821d601dfc8374379b8ec102571
Andrew Kolander <akolander22@gmail.com>
2016-11-21 13:45:07 -0600

    search functionality

--- public/scripts/MainController.js
+++ public/scripts/MainController.js
@@ -2,8 +2,8 @@ angular.module('deadApp').controller('MainController', function($http){
   var vm = this;
 
   vm.submit = function(){
-
-    console.log("searching for ", vm.search)
+    console.log("hit submit");
+    // console.log("searching for ", vm.search)
   }
 
 });
