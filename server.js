ff86ca616592c196aab8998a68fbb291bf1c0cbd
Andrew Kolander <akolander22@gmail.com>
2016-12-13 11:33:03 -0600

    connecting to mongo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-
+var mongoose = require('mongoose');
 
 
 var app = express();
@@ -10,6 +10,23 @@ app.use(express.static('public'));
 
 app.get('/', function(request, response){
   response.sendFile(path.join(__dirname, 'public/views/index.html'));
+})
+
+//mongodb
+var mongoURI = "";
+  if(process.env.MONGODB_URI != undefined){
+    // mongoURI = "mongodb://heroku_29xg3qd4:lj98hmokv852dgl7jt4facmpee@ds153765.mlab.com:53765/heroku_29xg3qd4";
+  } else {
+    mongoURI = "mongodb://localhost:27017/deadpool_users";
+  }
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function(err){
+  console.log('mongodb connection error', err);
+});
+
+MongoDB.once('open', function(){
+  console.log('mongodb connection open!!');
 })
 
 
