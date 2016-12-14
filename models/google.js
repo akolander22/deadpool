9eb6d72bff0b077bc3317fab33dc0c95e82175f9
Andrew Kolander <akolander22@gmail.com>
2016-12-14 13:10:40 -0600

    google auth

---
+++ models/google.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+
+    'googleAuth' : {
+        // 'clientID'      : '611248588736-k6b9koqbppqlhvqhkks3nf571rpp2cb0.apps.googleusercontent.com',
+        // 'clientSecret'  : 'jyehzd6jM0-v-QCmwF7fEsPP',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
