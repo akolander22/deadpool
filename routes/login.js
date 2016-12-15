fec74408ec92561d8cdbbf289f28a872efd6b1de
Andrew Kolander <akolander22@gmail.com>
2016-12-15 14:16:41 -0600

    register

--- routes/login.js
+++ routes/login.js
@@ -15,3 +15,25 @@ router.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
+
+
+router.get('/', function(request, response){
+  response.send(request.isAuthenticated());
+});
+
+router.get('/passportSuccess', function(req, res){
+  res.sendStatus(200);
+});
+
+router.get('/passportFailure', function(req, res){
+  res.sendStatus(401);
+});
+
+
+router.post('/', passport.authenticate('local', {
+  successRedirect: '/login/passportSuccess',
+  failureRedirect: '/login/passportFailure'
+}));
+
+
+module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -3,13 +3,40 @@ var path = require('path');
 var passport = require('passport');
 
 
-router.get('/signup', function(req, res) {
-			res.render('signup.ejs', { message: req.flash('loginMessage') });
+router.get('/register', function(req, res) {
+			res.render('register.html', { message: req.flash('loginMessage') });
 		});
 
-		// process the signup form
-		router.post('/register', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/register', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+// process the signup form
+router.post('/register', passport.authenticate('local-signup', {
+	successRedirect : '/profile', // redirect to the secure profile section
+	failureRedirect : '/register', // redirect back to the signup page if there is an error
+	failureFlash : true // allow flash messages
+}));
+
+var router = require('express').Router();
+var path = require('path');
+
+var User = require('../models/user');
+
+// router.get('/', function(request, response){
+//   response.sendFile(path.join(__dirname, '../public/views/register.html'));
+// });
+//
+//
+// router.post('/', function(request, response){
+//   console.log('Requested register!!!', new Date());
+//   User.create(request.body, function(err){
+//     if(err){
+//       console.log('HERE', err);
+//       response.sendStatus(500);
+//     } else {
+//       response.redirect('/');
+//     }
+//   })
+// })
+
+
+
+
+module.exports = router;
